5bbef43635c7c3a43e23bd6386ecf81fd7837527
Nadun Nissanka <61065783+Nadunnissanka@users.noreply.github.com>
2021-07-27 23:35:52 +0530

    jul/27/23:25 - error handling example

--- errors_and_exceptions.py
+++ errors_and_exceptions.py
@@ -12,3 +12,18 @@ else:
 finally:
     file.close()
     print("File was closed!")
+
+# Example 01
+fruits = ["Apple", "Pear", "Orange"]
+
+
+# TODO: Catch the exception and make sure the code runs without crashing.
+def make_pie(index):
+    try:
+        fruit = fruits[index]
+        print(fruit + " pie")
+    except IndexError:
+        print("fruit pie")
+
+
+make_pie(4)
